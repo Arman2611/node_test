d19c0e500d0858cdb0052149e1d079db8bad2629
Arman <arman2611m@gmail.com>
2022-03-25 01:39:01 +0400

    Added nodemon

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/', async (req, res) => {
 	res.status(200).send('working...')
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('Starting server on port 3000')
 })
